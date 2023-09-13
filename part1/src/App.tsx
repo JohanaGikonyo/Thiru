87d1551bf08ad98bd611c2e89ed8a255782e27de
JohanaGikonyo <johanagikonyo552@gmail.com>
2023-09-13 11:57:08 +0300

    first commit

---
+++ part1/src/App.tsx
@@ -0,0 +1,17 @@
+import './Header'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+   <div>
+   <header />
+  <h1>First React App</h1>
+  </div>
+    </>
+  )
+}
+
+export default App
